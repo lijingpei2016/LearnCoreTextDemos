22c9bc281f0181ecbca1cd42cef5af7ad4bb1e6e
李境沛 <lijingpei@inke.cn>
2021-06-29 10:24:14 +0800

    ADD：添加注释

--- 01_helloCoreText/01_helloCoreText/JPLabel.m
+++ 01_helloCoreText/01_helloCoreText/JPLabel.m
@@ -11,15 +11,21 @@
 @implementation JPLabel
 
 - (void)drawRect:(CGRect)rect {
-    
     CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+    //matrix 矩阵 affine transform identity
+//    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+    //在Y轴上移动 height个单位   coretext的原点是在左下角  而我们平时的ui控件是在左上角
+    //Translate 翻译
     CGContextTranslateCTM(context, 0, self.bounds.size.height);
+
+    //Changes the scale of the user coordinate system in a context.
+    //在上下文中更改用户坐标系的比例。
     CGContextScaleCTM(context, 1, -1);
 
     // 绘制区域
     CGMutablePathRef path = CGPathCreateMutable();
     CGPathAddRect(path, NULL, self.bounds);
+    
 
     // 绘制的内容属性字符串
     NSDictionary *attributes = @{ NSFontAttributeName: [UIFont systemFontOfSize:18],
