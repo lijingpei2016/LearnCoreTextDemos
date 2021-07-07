6ec93788b1965bd1b16528f2258713cabae7a5b9
李境沛 <as330732842@qq.com>
2021-07-07 11:28:33 +0800

    ADD:添加自己的感悟

--- 01_helloCoreText/01_helloCoreText/JPLabel.m
+++ 01_helloCoreText/01_helloCoreText/JPLabel.m
@@ -11,36 +11,33 @@
 @implementation JPLabel
 
 - (void)drawRect:(CGRect)rect {
+    //创建一个上下文
     CGContextRef context = UIGraphicsGetCurrentContext();
-    //matrix 矩阵 affine transform identity
-//    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+
     //在Y轴上移动 height个单位   coretext的原点是在左下角  而我们平时的ui控件是在左上角
-    //Translate 翻译
     CGContextTranslateCTM(context, 0, self.bounds.size.height);
 
-    //Changes the scale of the user coordinate system in a context.
     //在上下文中更改用户坐标系的比例。
     CGContextScaleCTM(context, 1, -1);
 
     // 绘制区域
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, self.bounds);
-    
 
-    // 绘制的内容属性字符串
+    //第二个参数是Transform
+    CGPathAddRect(path, NULL, self.bounds);
+
+    // 设置配置的字典 比如字体 颜色
     NSDictionary *attributes = @{ NSFontAttributeName: [UIFont systemFontOfSize:18],
                                   NSForegroundColorAttributeName: [UIColor orangeColor] };
     NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:@"Hello CoreText Hello CoreText" attributes:attributes];
 
-    // 使用NSMutableAttributedString创建CTFrame
+    // 存储材料，渲染的frame是根据渲染材料的完整度而得出的，比如我渲染一半或者百分之10，得出来的frame可能是不一样的
     CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attrStr);
+    //这里我的理解是，CTFramesetterRef提供内容，记录了所有文字（要渲染）内容，计算出来的是需要渲染的范围，比如我可以只渲染内容里的一部分，之所以要传一个path是因为我要确定宽，才可以得出高
     CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, attrStr.length), path, NULL);
 
-    // 使用CTFrame在CGContextRef上下文上绘制
+    // 在context这个画布里，渲染frame的内容，  相当于context就是一个窗口，frame是幅画的内容，窗口可能可以看到部分、完整、恰好相等的画，如果窗口小，画大，那就只能看到一部分
     CTFrameDraw(frame, context);
-    
-    
-    
 }
 
 @end
